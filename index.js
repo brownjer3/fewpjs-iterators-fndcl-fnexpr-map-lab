f2a649d3281e40f1c0f5dc2ab909d4adb2b10835
jerry <brownjer3@gmail.com>
2021-02-05 14:22:36 -0800

    commit tree 297d74ad15f99fd1e6b517630e57a4960231ddd7

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(t => {
+    let upperCased = t.split(" ").map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    }).join(" ")
+    return upperCased
+  })
 }
